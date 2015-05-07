0d4eed37668c17f6533e86207adc7a30ade45408
daveraskin <draskal2@gmail.com>
2015-05-07 11:07:46 -0700

    yeah yeah

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507171239) do
+ActiveRecord::Schema.define(version: 20150507175641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150507171239) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "min"
+    t.integer  "day"
   end
 
   add_index "events", ["group_id"], name: "index_events_on_group_id", using: :btree
