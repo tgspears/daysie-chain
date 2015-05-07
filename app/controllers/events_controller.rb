cc3d1b8d529dfd35e86f80e458e0a2bcf9d2d238
daveraskin <draskal2@gmail.com>
2015-05-07 16:15:28 -0700

    switched the order of scripts

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,16 @@
 class EventsController < ApplicationController
 
   def create
-    render :json => params
+    user = User.find(params[:user_id])
+    group = user.groups.find(params[:group_id])
+    group.events << Event.create(event_params)
+
   end
 
+
+  private
+
+  def event_params
+    params.require(:new_event).permit(:name, :desc, :max, :loc, :date)
+  end
 end
