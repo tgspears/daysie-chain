975246c71c49e9ea52f6dd89114fbde1ed79946f
Alexander Nguyen <nguyen.h.alexander@gmail.com>
2015-05-15 00:44:41 -0700

    created about section in accordion

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -151,6 +151,18 @@ $(function(){
 
   $("body").tooltip({ selector: '[data-toggle=tooltip]' });
 
+$('#collapseOne').on('show.bs.collapse', function () {
+    $('.panel-heading').animate({
+        backgroundColor: "#515151"
+    }, 500);
+})
+
+$('#collapseOne').on('hide.bs.collapse', function () {
+    $('.panel-heading').animate({
+        backgroundColor: "#00B4FF"
+    }, 500);
+})
+
 })
 
 
