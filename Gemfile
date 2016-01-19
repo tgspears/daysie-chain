935e2895c3b0c14aba383eaf5ecbea2d2173641b
Trevor Spears <tg.spears@gmail.com>
2016-01-19 09:22:59 -0800

    made secrets secret, setting up twilio

--- Gemfile
+++ Gemfile
@@ -56,6 +56,10 @@ gem 'seed_dump'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'rails_12factor', group: :production
+
+ruby "2.2.1"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
@@ -68,4 +72,3 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,53 +1,147 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!([
-  {firstname: "Aida", lastname: "Mengistu", email: "aida@mengistu.com", tel: "12063711202", password_digest: nil, provider_id: nil, provider: nil, provider_hash: nil, image: nil, active: nil},
-  {firstname: "Allie", lastname: "Moses", email: "allie@moses.com", tel: "15305267767", password_digest: nil, provider_id: nil, provider: nil, provider_hash: nil, image: nil, active: nil},
-  {firstname: "Ben", lastname: "Piggot", email: "ben@piggot.com", tel: "12062513317", password_digest: nil, provider_id: nil, provider: nil, provider_hash: nil, image: nil, active: nil},
-  {firstname: "Bennett", lastname: "Lin", email: "bennett@lin.com", tel: "18183576453", password_digest: nil, provider_id: nil, provider: nil, provider_hash: nil, image: nil, active: nil},
-  {firstname: "Cody", lastname: "Barrus", email: "cody@barrus.com", tel: "14252954463", password_digest: nil, provider_id: nil, provider: nil, provider_hash: nil, image: nil, active: nil},
-  {firstname: "Dan", lastname: "McGough", email: "dan@mcgough.com", tel: "12069402064", password_digest: nil, provider_id: nil, provider: nil, provider_hash: nil, image: nil, active: nil},
-  {firstname: "Daniel", lastname: "Smith", email: "daniel@smith.com", tel: "13605958585", password_digest: nil, provider_id: nil, provider: nil, provider_hash: nil, image: nil, active: nil},
-  {firstname: "Greg", lastname: "Wyszynski", email: "greg@wyszynski.com", tel: "14406684858", password_digest: nil, provider_id: nil, provider: nil, provider_hash: nil, image: nil, active: nil},
-  {firstname: "Jake", lastname: "Rohr", email: "jake@rohr.com", tel: "12069488340", password_digest: nil, provider_id: nil, provider: nil, provider_hash: nil, image: nil, active: nil},
-  {firstname: "Katie", lastname: "Gilmur", email: "katie@gilmur.com", tel: "12537205447", password_digest: nil, provider_id: nil, provider: nil, provider_hash: nil, image: nil, active: nil},
-  {firstname: "Lauren", lastname: "Go", email: "lauren@go.com", tel: "16507034555", password_digest: nil, provider_id: nil, provider: nil, provider_hash: nil, image: nil, active: nil},
-  {firstname: "Matt", lastname: "Kiser", email: "matt@kiser.com", tel: "17077387948", password_digest: nil, provider_id: nil, provider: nil, provider_hash: nil, image: nil, active: nil},
-  {firstname: "Nick", lastname: "Cronquist", email: "nick@cronquist.com", tel: "12068861432", password_digest: nil, provider_id: nil, provider: nil, provider_hash: nil, image: nil, active: nil},
-  {firstname: "Patrick", lastname: "Eldridge", email: "patrick@eldridge.com", tel: "14055179714", password_digest: nil, provider_id: nil, provider: nil, provider_hash: nil, image: nil, active: nil},
-  {firstname: "Riley", lastname: "Joseph", email: "riley@joseph.com", tel: "14058023368", password_digest: nil, provider_id: nil, provider: nil, provider_hash: nil, image: nil, active: nil},
-  {firstname: "Sean", lastname: "Villagracia", email: "sean@villagracia.com", tel: "19162260509", password_digest: nil, provider_id: nil, provider: nil, provider_hash: nil, image: nil, active: nil},
-  {firstname: "Zach", lastname: "Huntting", email: "zach@huntting.com", tel: "12064995653", password_digest: nil, provider_id: nil, provider: nil, provider_hash: nil, image: nil, active: nil},
-  {firstname: "Sarah", lastname: "Hanley", email: "sarah@hanley.com", tel: "14436314900", password_digest: nil, provider_id: nil, provider: nil, provider_hash: nil, image: nil, active: nil},
-  {firstname: "Trevor", lastname: "Spears", email: "tg.spears@gmail.com", tel: "14252050766", password_digest: "$2a$10$5ke3M.RfEPh1aYaehmQnTuSSRE0qU5hvJJPEGwreYt9oB0D2KbYcO", provider_id: nil, provider: nil, provider_hash: nil, image: "gecopflarqsddjgwpwiw", active: nil}
-])
 
-Group.create!([
-  {name: "WDI-SEA-02", size: 25, status: nil, user_id: 20, image: "emvorewykhyskqh1jzmm"}
-])
-Event.create!([
-  {name: "Demo Day!", desc: "Time to show off our hard work!", max: 25, loc: "Classroom 1", date: nil, time: nil, group_id: 1, min: nil, day: nil, active: nil}
-])
-Membership.create!([
-  {group_id: 1, user_id: 20, admin: true, block: nil},
-  {group_id: 1, user_id: 2, admin: false, block: nil},
-  {group_id: 1, user_id: 3, admin: true, block: nil},
-  {group_id: 1, user_id: 4, admin: false, block: nil},
-  {group_id: 1, user_id: 5, admin: false, block: nil},
-  {group_id: 1, user_id: 6, admin: false, block: nil},
-  {group_id: 1, user_id: 7, admin: false, block: nil},
-  {group_id: 1, user_id: 8, admin: false, block: nil},
-  {group_id: 1, user_id: 9, admin: false, block: nil},
-  {group_id: 1, user_id: 11, admin: false, block: nil},
-  {group_id: 1, user_id: 12, admin: false, block: nil},
-  {group_id: 1, user_id: 13, admin: false, block: nil},
-  {group_id: 1, user_id: 14, admin: false, block: nil},
-  {group_id: 1, user_id: 15, admin: false, block: nil},
-  {group_id: 1, user_id: 16, admin: false, block: nil},
-  {group_id: 1, user_id: 17, admin: false, block: nil},
-  {group_id: 1, user_id: 18, admin: false, block: nil},
-  {group_id: 1, user_id: 19, admin: false, block: nil},
-  {group_id: 1, user_id: 10, admin: true, block: nil},
-  {group_id: 1, user_id: 1, admin: false, block: nil}
+#################### USERS ######################
 
-])
+User.create(firstname: 'Trevor', lastname: 'Spears', email: 'trevor@spears.com', tel: '14252050766', password: 'trevorspears', image: 'http://static9.cdn.ubi.com/en-GB/images/LARP_2012_2tcm2158734.png')
 
+User.create(firstname: 'Lauren', lastname: 'Go', email: 'lauren@go.com', tel: '14252050766', password: 'laurengo', image: 'http://www.peopleofwalmart.com/wp-content/uploads/2015/05/553.jpg')
+
+User.create(firstname: 'Dave', lastname: 'Raskin', email: 'dave@raskin.com', tel: '14252050766', password: 'daveraskin', image: 'http://www.peopleofwalmart.com/wp-content/uploads/2015/05/554.jpg')
+
+User.create(firstname: 'Alex', lastname: 'Nguyen', email: 'alex@nguyen.com', tel: '14252050766', password: 'alexnguyen', image: 'http://www.peopleofwalmart.com/wp-content/uploads/2015/05/549.jpg')
+
+User.create(firstname: 'Mark', lastname: 'Bradford', email: 'mark@bradford.com', tel: '14252050766', password: 'markbradford', image: 'http://www.peopleofwalmart.com/wp-content/uploads/2015/05/550.jpg')
+
+User.create(firstname: 'Timon', lastname: 'Pike', email: 'timon@pike.com', tel: '14252050766', password: 'timonpike', image: 'http://www.peopleofwalmart.com/wp-content/uploads/2015/05/545.jpg')
+
+User.create(firstname: 'Nick', lastname: 'Cronquist', email: 'nick@cronquist.com', tel: '14252050766', password: 'nickcronquist', image: 'http://www.peopleofwalmart.com/wp-content/uploads/2015/05/546.jpg')
+
+################### GROUPS #####################
+
+Group.create(name: "Trevor's Group", size: 10, status: true, user_id: 1, image: 'http://static9.cdn.ubi.com/en-GB/images/LARP_2012_2tcm2158734.png')
+
+Group.create(name: "Lauren's Group", size: 12, status: true, user_id: 2, image: 'https://www.bewelcome.org/groups/realimg/451')
+
+Group.create(name: "Dave's Group", size: 5, status: false, user_id: 3, image: 'http://cdn2-b.examiner.com/sites/default/files/styles/image_content_width/hash/a8/c0/a8c04b45b3a7282b874b2473e77c32a9.jpg?itok=UTaG1AAP')
+
+Group.create(name: "Alex's Group", size: 420, status: false, user_id: 4, image: 'http://usercontent1.hubimg.com/2213864_f496.jpg')
+
+Group.create(name: "Mark's Group", size: 8, status: true, user_id: 5, image: 'http://www.veryfunnycats.info/wp-content/uploads/2007/02/funnycatsgroup1.jpg')
+
+Group.create(name: "Timon's Group", size: 42, status: true, user_id: 6, image: 'http://twistedsifter.files.wordpress.com/2010/11/street-fighter-mortal-kombat-group-funny-halloween-costume.jpg')
+
+################### MEMBERSHIPS ##########################
+
+#----------------TREVOR------------------#
+Membership.create(group_id: 1, user_id: 1, admin: true, block: false)
+Membership.create(group_id: 1, user_id: 2, admin: false, block: true)
+Membership.create(group_id: 1, user_id: 3, admin: true, block: false)
+Membership.create(group_id: 1, user_id: 4, admin: true, block: true)
+Membership.create(group_id: 1, user_id: 5, admin: true, block: false)
+Membership.create(group_id: 1, user_id: 6, admin: false, block: false)
+Membership.create(group_id: 1, user_id: 7, admin: false, block: false)
+
+#----------------LAUREN------------------#
+Membership.create(group_id: 2, user_id: 1, admin: false, block: false)
+Membership.create(group_id: 2, user_id: 2, admin: true, block: false)
+Membership.create(group_id: 2, user_id: 3, admin: false, block: true)
+Membership.create(group_id: 2, user_id: 4, admin: false, block: false)
+Membership.create(group_id: 2, user_id: 5, admin: true, block: false)
+Membership.create(group_id: 2, user_id: 6, admin: false, block: false)
+
+#----------------DAVE------------------#
+Membership.create(group_id: 3, user_id: 1, admin: true, block: false)
+Membership.create(group_id: 3, user_id: 2, admin: true, block: false)
+Membership.create(group_id: 3, user_id: 3, admin: true, block: false)
+Membership.create(group_id: 3, user_id: 4, admin: false, block: true)
+Membership.create(group_id: 3, user_id: 5, admin: false, block: false)
+
+#----------------ALEX------------------#
+Membership.create(group_id: 4, user_id: 1, admin: true, block: false)
+Membership.create(group_id: 4, user_id: 3, admin: true, block: false)
+Membership.create(group_id: 4, user_id: 4, admin: true, block: false)
+Membership.create(group_id: 4, user_id: 5, admin: false, block: false)
+
+
+#----------------MARK------------------#
+Membership.create(group_id: 5, user_id: 5, admin: true, block: false)
+Membership.create(group_id: 5, user_id: 4, admin: false, block: false)
+Membership.create(group_id: 5, user_id: 2, admin: false, block: false)
+
+#----------------TIMON------------------#
+Membership.create(group_id: 6, user_id: 6, admin: true, block: false)
+Membership.create(group_id: 6, user_id: 1, admin: false, block: false)
+
+#----- Nick is the user who is not signed up ------#
+
+################## EVENTS ######################
+
+Event.create(name: "Trevor's Event", desc: 'An Event by Trevor', min: 5, max: 10, loc: "Trevor's Place", date: '1/2', day: 0, time: '1:30', group_id: 1)
+
+Event.create(name: "Trevor's Event 2", desc: 'An Event by Trevor 2', min: 5, max: 10, loc: "Trevor's Place", date: '2/1', day: 0, time: '2:30', group_id: 1)
+
+Event.create(name: "Lauren's Event", desc: 'An Event by Lauren', min: 4, max: 11, loc: "Lauren's Place", date: '2/3', day: 1, time: '2:30', group_id: 2)
+
+Event.create(name: "Dave's Event", desc: 'An Event by Dave', min: 6, max: nil, loc: "Dave's Place", date: '3/4', day: 2, time: '3:00', group_id: 3)
+
+Event.create(name: "Alex's Event", desc: 'An Event by Alex', min: 2, max: 421, loc: "Alex's Place", date: '4/2', day: 3, time: '4:20', group_id: 4)
+
+Event.create(name: "Mark's Event", desc: 'An Event by Mark', min: 4, max: 8, loc: "Mark's Place", date: '4/2', day: 4, time: '4:20', group_id: 5)
+########### ^^^ TEST OF TWO IDENTICLE TIMES AND DATES
+
+Event.create(name: "Timon's Event", desc: 'An Event by Timon', min: 5, max: 10, loc: "Timon's Place", date: '3/14', day: 5, time: '6:28', group_id: 6)
+
+################# ATTENDANCES ###################
+
+#---------------- TREVOR --------------------#
+
+Attendance.create(event_id: 1, user_id: 1, yes: true, maybe: false, no: false)
+Attendance.create(event_id: 1, user_id: 2, yes: true, maybe: false, no: false)
+Attendance.create(event_id: 1, user_id: 3, yes: true, maybe: false, no: false)
+Attendance.create(event_id: 1, user_id: 4, yes: true, maybe: false, no: false)
+Attendance.create(event_id: 1, user_id: 5, yes: false, maybe: true, no: false)
+Attendance.create(event_id: 1, user_id: 6, yes: false, maybe: false, no: true)
+Attendance.create(event_id: 1, user_id: 7, yes: false, maybe: true, no: false)
+
+#----------------- LAUREN -------------------#
+
+Attendance.create(event_id: 2, user_id: 2, yes: true, maybe: false, no: false)
+Attendance.create(event_id: 2, user_id: 3, yes: true, maybe: false, no: false)
+Attendance.create(event_id: 2, user_id: 4, yes: false, maybe: true, no: false)
+Attendance.create(event_id: 2, user_id: 5, yes: true, maybe: false, no: false)
+Attendance.create(event_id: 2, user_id: 6, yes: false, maybe: false, no: true)
+Attendance.create(event_id: 2, user_id: 1, yes: true, maybe: false, no: false)
+
+#------------------- DAVE ---------------------#
+
+Attendance.create(event_id: 3, user_id: 3, yes: true, maybe: false, no: false)
+Attendance.create(event_id: 3, user_id: 4, yes: true, maybe: false, no: false)
+Attendance.create(event_id: 3, user_id: 5, yes: true, maybe: false, no: false)
+Attendance.create(event_id: 3, user_id: 2, yes: true, maybe: false, no: false)
+Attendance.create(event_id: 3, user_id: 1, yes: true, maybe: false, no: true)
+############ ^^^ TEST OF 2 TRUES
+
+#-------------------- ALEX ----------------------#
+
+Attendance.create(event_id: 4, user_id: 4, yes: false, maybe: false, no: false)
+############################################ ^^^ NOT GOING TO OWN EVENT
+Attendance.create(event_id: 4, user_id: 1, yes: true, maybe: false, no: false)
+Attendance.create(event_id: 4, user_id: 5, yes: true, maybe: false, no: false)
+Attendance.create(event_id: 4, user_id: 3, yes: true, maybe: false, no: false)
+
+#------------------ MARK ---------------------#
+
+Attendance.create(event_id: 5, user_id: 5, yes: true, maybe: false, no: false)
+Attendance.create(event_id: 5, user_id: 4, yes: false, maybe: false, no: true)
+Attendance.create(event_id: 5, user_id: 1, yes: true, maybe: false, no: false)
+################################# ^^^ ATTENDING NOT IN GROUP
+
+Attendance.create(event_id: 6, user_id: 6, yes: true, maybe: false, no: false)
+Attendance.create(event_id: 6, user_id: 1, yes: true, maybe: false, no: false)
